e543d24c8d406766f37ec0dfe0fe2938cb9eb185
Valpik97 <114172485+Valpik97@users.noreply.github.com>
2023-02-02 18:40:45 +0300

    Add files via upload

---
+++ iterators and generators.py
@@ -0,0 +1,79 @@
+from typing import Callable, List, Any
+from time import sleep
+
+
+class Repeater:
+    def __init__(self, value: Any):
+        self.value: Any = value
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Any:
+        return self.value
+
+
+class Count:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.value: int = start
+        self.end = end
+
+    def __iter__(self):
+        self.value = self.start
+        return self
+
+    def __next__(self) -> Any:
+        if self.value == self.end:
+            print("raising StopIteration")
+            raise StopIteration
+        self.value += 1
+        return self.value
+
+
+# data1 = [3, 4, 5, 6]
+# data = Repeater(data1)
+data = Count(5, 10)
+for x in data:
+    print(x)
+    sleep(1)
+
+data = Count(5, 10)
+for x in data:
+    print(x)
+    sleep(1)
+
+# iterator = iter(data)
+# print(iterator)
+#
+# while True:
+#     x = None
+#     try:
+#         x = next(iterator)
+#     except StopIteration:
+#         break
+#     print(x)
+
+
+# таймер
+# class Count:
+#     def __init__(self, start: int, end: int):
+#         self.start = start
+#         self.value: int = start
+#         self.end = end
+#
+#     def __iter__(self):
+#         self.value = self.start
+#         return self
+#
+#     def __next__(self) -> Any:
+#         if self.value == self.end:
+#             print("raising StopIteration")
+#             return
+#         self.value += 1
+#         return self.value
+
+# data = Count(5, 10)
+# for x in data:
+#     print(x)
+#     sleep(1)
